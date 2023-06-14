bbd0a77fb93057027db76ac83d9dd85a0a74115a
ValenRomero <valenromero678@gmail.com>
2023-06-14 11:48:23 -0300

    creamos login y registro

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagesComponent,
+    LoginComponent,
+    RegistroComponent
+  ],
   imports: [
     CommonModule
   ]
